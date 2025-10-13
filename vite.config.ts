dcf0ef582b57af6a4f6802b129b22f02df6eba37
hramn1 <stmihailov@yandex.ru>
2025-10-14 01:02:56 +0300

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// !!! вот это ключевая строка:
 export default defineConfig({
+  base: '/test-skyeng/',
   plugins: [react()],
 })
